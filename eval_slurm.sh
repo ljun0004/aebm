e8eef8747745452f2e62dd4b65667545fe7c6468
Junn <109283164+ljun0004@users.noreply.github.com>
2026-03-04 15:57:35 +0800

    Update VAE paths in eval_slurm.sh

--- eval_slurm.sh
+++ eval_slurm.sh
@@ -5,17 +5,9 @@
 
 # Define paths
 IMAGENET_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/train"
-
-# CACHED_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/cache_kl16"
-# VAE_CKPT="/home/ljun0004/sa31/Junn/mar/pretrained_models/vae/kl16.ckpt"
-# VAE_CFG="/home/ljun0004/sa31/Junn/mar/pretrained_models/vae/kl16.ckpt"
-
-# CACHED_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/cache_vq-f8"
-# VAE_CKPT="/home/ljun0004/sa31/Junn/bmar/pretrained_models/vae/vq-f8.ckpt"
-# VAE_CFG="/home/ljun0004/sa31/Junn/bmar/first_stage_models/vq-f8/config.yaml"
-
 CACHED_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/cache_vq-f8-n256"
-VAE_CKPT="/home/ljun0004/sa31/Junn/bmar/pretrained_models/vae/vq-f8-n256.ckpt"
+
+VAE_PATH="/home/ljun0004/sa31/Junn/bmar/pretrained_models/vae/vq-f8-n256.ckpt"
 VAE_CFG="/home/ljun0004/sa31/Junn/bmar/first_stage_models/vq-f8-n256/config.yaml"
 
 LOAD_PATH="/home/ljun0004/sa31/Junn/bmar/checkpoints/pretrain/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask32x32_seqlen16x16_zprojtied_encembed_wmlpratio4_rescon_L2norm_wu0_wd0.02_gc3"
@@ -34,7 +26,7 @@ torchrun \
     main_mar.py \
     --img_size 256 \
     --vae_mode vq \
-    --vae_ckpt ${VAE_CKPT} \
+    --vae_path ${VAE_PATH} \
     --vae_cfg ${VAE_CFG} \
     --vae_embed_dim 4 \
     --vae_stride 8 \
