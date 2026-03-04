46f5526414cc42c9148e6d46962f4227a3333d3c
Junn <109283164+ljun0004@users.noreply.github.com>
2026-03-04 15:56:21 +0800

    Update VAE paths and remove commented lines

--- cache_slurm.sh
+++ cache_slurm.sh
@@ -5,17 +5,9 @@
 
 # Define paths
 IMAGENET_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/train"
-
-# CACHED_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/cache_kl16"
-# VAE_CKPT="/home/ljun0004/sa31/Junn/mar/pretrained_models/vae/kl16.ckpt"
-# VAE_CFG="/home/ljun0004/sa31/Junn/mar/pretrained_models/vae/kl16.ckpt"
-
-# CACHED_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/cache_vq-f8"
-# VAE_CKPT="/home/ljun0004/sa31/Junn/bmar/pretrained_models/vae/vq-f8.ckpt"
-# VAE_CFG="/home/ljun0004/sa31/Junn/bmar/first_stage_models/vq-f8/config.yaml"
-
 CACHED_PATH="/home/ljun0004/sa31_scratch2/Junn/datasets/imagenet/cache_vq-f8-n256"
-VAE_CKPT="/home/ljun0004/sa31/Junn/bmar/pretrained_models/vae/vq-f8-n256.ckpt"
+
+VAE_PATH="/home/ljun0004/sa31/Junn/bmar/pretrained_models/vae/vq-f8-n256.ckpt"
 VAE_CFG="/home/ljun0004/sa31/Junn/bmar/first_stage_models/vq-f8-n256/config.yaml"
 
 # Run training
@@ -28,7 +20,7 @@ torchrun \
     main_cache.py \
     --img_size 256 \
     --vae_mode vq \
-    --vae_ckpt ${VAE_CKPT} \
+    --vae_path ${VAE_PATH} \
     --vae_cfg ${VAE_CFG} \
     --vae_embed_dim 4 \
     --batch_size 256 \
