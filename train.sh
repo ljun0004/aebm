aae12ccab411f2077771b50f2c22ed17343463de
Junn <109283164+ljun0004@users.noreply.github.com>
2026-03-22 23:31:53 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -4,25 +4,25 @@
 ## -----------------------------
 ## Path Definitions
 ## -----------------------------
-PROJECT_ROOT="/home/junn/Junn/aebm"
+PROJECT_ROOT="/root/autodl-tmp"
 IMAGENET_PATH="${PROJECT_ROOT}/datasets/imagenet/train"
 CACHED_PATH="${PROJECT_ROOT}/datasets/imagenet/cached/vq-f8-n256"
 VAE_PATH="${PROJECT_ROOT}/pretrained_models/vq-f8-n256/model.ckpt"
 VAE_CFG="${PROJECT_ROOT}/aebm/first_stage_models/vq-f8-n256/config.yaml"
-LOAD_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_zprojtied_wresmlp_L2norm_blr1.5e-4_sqrt_wu50_wd0.05_gc3/checkpoint-last.pth"
-SAVE_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_zprojtied_wresmlp_L2norm_blr1.5e-4_sqrt_wu50_wd0.05_gc3"
+LOAD_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_zprojtied_wresmlp_L2norm_blr2e-4_sqrt_wu50_wd0.05_gc3_bsz1600/checkpoint-last.pth"
+SAVE_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_zprojtied_wresmlp_L2norm_blr2e-4_sqrt_wu50_wd0.05_gc3_bsz1600"
 LOG_PATH="${PROJECT_ROOT}/logs"
 
 ## -----------------------------
 ## Automated Logging
 ## -----------------------------
-# mkdir -p "${LOG_PATH}"
-# LOG_FILE="${LOG_PATH}/train_$(date +%Y%m%d_%H%M%S).txt"
-# exec > >(tee -a "${LOG_FILE}") 2>&1
-# echo "========================================"
-# echo " Job Started: $(date)"
-# echo " Log file: ${LOG_FILE}"
-# echo "========================================"
+mkdir -p "${LOG_PATH}"
+LOG_FILE="${LOG_PATH}/train_$(date +%Y%m%d_%H%M%S).txt"
+exec > >(tee -a "${LOG_FILE}") 2>&1
+echo "========================================"
+echo " Job Started: $(date)"
+echo " Log file: ${LOG_FILE}"
+echo "========================================"
 
 ## -----------------------------
 ## Environment Setup
@@ -76,12 +76,12 @@ torchrun \
     --vae_stride 8 \
     --patch_size 2 \
     --model mar_base \
-    --batch_size 128 \
-    --accum_iter 1 \
+    --batch_size 200 \
+    --accum_iter 2 \
     --num_workers 16 \
     --epochs 100 \
     --warmup_epochs 50 \
-    --blr 1.5e-4 \
+    --blr 2.0e-4 \
     --weight_decay 0.05 \
     --grad_clip 3.0 \
     --alpha 1.0 \
@@ -95,6 +95,7 @@ torchrun \
     --mask_ratio_mu 1.00 \
     --mask_ratio_std 0.25 \
     --data_path ${IMAGENET_PATH} \
+    --cached_path ${CACHED_PATH} \
     --resume ${LOAD_PATH} \
     --output_dir ${SAVE_PATH} \
     --save_freq 10 \
@@ -102,7 +103,6 @@ torchrun \
     --encoder_adaln_mod \
     --decoder_adaln_mod \
     --final_layer_adaln_mod \
-    --cached_path ${CACHED_PATH} \
     --use_cached
     # --grad_checkpointing
 
