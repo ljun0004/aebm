aaed719ecf961ae9920c144b535b39db1029f310
Junn <109283164+ljun0004@users.noreply.github.com>
2026-03-28 14:32:03 +0800

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -9,8 +9,8 @@ IMAGENET_PATH="${PROJECT_ROOT}/datasets/imagenet/train"
 CACHED_PATH="${PROJECT_ROOT}/datasets/imagenet/cached/vq-f8-n256"
 VAE_PATH="${PROJECT_ROOT}/pretrained_models/vq-f8-n256/model.ckpt"
 VAE_CFG="${PROJECT_ROOT}/aebm/first_stage_models/vq-f8-n256/config.yaml"
-LOAD_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_zprojtied_wresmlp_L2norm_blr2e-4_sqrt_wu50_wd0.05_gc3_bsz1024/checkpoint-last.pth"
-SAVE_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_zprojtied_wresmlp_L2norm_blr2e-4_sqrt_wu50_wd0.05_gc3_bsz1024"
+LOAD_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_learnemb_zprojtied_wresmlp_L2norm_blr1e-4_linear_wu0_wd0.02_gc3_bsz1024/checkpoint-last.pth"
+SAVE_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_learnemb_zprojtied_wresmlp_L2norm_blr1e-4_linear_wu0_wd0.02_gc3_bsz1024"
 LOG_PATH="${PROJECT_ROOT}/logs"
 
 ## -----------------------------
@@ -77,7 +77,7 @@ torchrun \
     --patch_size 2 \
     --model mar_base \
     --batch_size 64 \
-    --accum_iter 1 \
+    --accum_iter 2 \
     --num_workers 16 \
     --epochs 100 \
     --warmup_epochs 50 \
