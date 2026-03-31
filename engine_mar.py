4aadff7c191817cb5db2f144091aaa66a7c7409c
Junn <109283164+ljun0004@users.noreply.github.com>
2026-03-31 22:52:45 +0800

    Refactor FID and IS computation logic

--- engine_mar.py
+++ engine_mar.py
@@ -285,25 +285,25 @@ def evaluate(model_without_ddp, vae, ema_params, args, epoch, batch_size=16, log
 
     # compute FID and IS
     if misc.get_rank() == 0:
+        if args.img_size == 256:
+            input2 = None
+            fid_statistics_file = 'fid_stats/adm_in256_stats.npz'
+        else:
+            raise NotImplementedError
+        metrics_dict = torch_fidelity.calculate_metrics(
+            input1=save_folder,
+            input2=input2,
+            fid_statistics_file=fid_statistics_file,
+            cuda=True,
+            isc=True,
+            fid=True,
+            kid=False,
+            prc=False,
+            verbose=False,
+        )
+        fid = metrics_dict['frechet_inception_distance']
+        inception_score = metrics_dict['inception_score_mean']
         if log_writer is not None:
-            if args.img_size == 256:
-                input2 = None
-                fid_statistics_file = 'fid_stats/adm_in256_stats.npz'
-            else:
-                raise NotImplementedError
-            metrics_dict = torch_fidelity.calculate_metrics(
-                input1=save_folder,
-                input2=input2,
-                fid_statistics_file=fid_statistics_file,
-                cuda=True,
-                isc=True,
-                fid=True,
-                kid=False,
-                prc=False,
-                verbose=False,
-            )
-            fid = metrics_dict['frechet_inception_distance']
-            inception_score = metrics_dict['inception_score_mean']
             postfix = ""
             if use_ema:
                 postfix = postfix + "_ema"
@@ -311,9 +311,9 @@ def evaluate(model_without_ddp, vae, ema_params, args, epoch, batch_size=16, log
                 postfix = postfix + "_cfg{}".format(cfg)
             log_writer.add_scalar('fid{}'.format(postfix), fid, epoch)
             log_writer.add_scalar('is{}'.format(postfix), inception_score, epoch)
-            print("FID: {:.4f}, Inception Score: {:.4f}".format(fid, inception_score))
-            # remove temporal saving folder
-            # shutil.rmtree(save_folder)
+        print("FID: {:.4f}, Inception Score: {:.4f}".format(fid, inception_score))
+        # remove temporal saving folder
+        # shutil.rmtree(save_folder)
 
     torch.distributed.barrier()
     time.sleep(10)
