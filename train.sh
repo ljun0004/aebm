1fee76bd59a6299b581c5ee7931a04b1704364ae
Junn <109283164+ljun0004@users.noreply.github.com>
2026-04-04 12:35:32 +0800

    Modify model paths in train.sh
    Updated LOAD_PATH and SAVE_PATH with new model configuration.

--- train.sh
+++ train.sh
@@ -9,8 +9,8 @@ IMAGENET_PATH="${PROJECT_ROOT}/datasets/imagenet/train"
 CACHED_PATH="${PROJECT_ROOT}/datasets/imagenet/cached/vq-f8-n256"
 VAE_PATH="${PROJECT_ROOT}/pretrained_models/vq-f8-n256/model.ckpt"
 VAE_CFG="${PROJECT_ROOT}/aebm/first_stage_models/vq-f8-n256/config.yaml"
-LOAD_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_learnemb_zprojtied_wresmlp_L2norm_blr1e-4_linear_wu0_wd0.02_gc3_bsz512/checkpoint-last.pth"
-SAVE_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_learnemb_zprojtied_wresmlp_L2norm_blr1e-4_linear_wu0_wd0.02_gc3_bsz512"
+LOAD_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_learnemb_zprojtied_wresmlp_L2norm_logitbias_blr1e-4_linear_wu0_wd0.02_gc3_bsz512/checkpoint-last.pth"
+SAVE_PATH="${PROJECT_ROOT}/ckpts/vq-f8-n256/mar_base/masked_alpha1.0_beta1.0_ddpm1.0_ce1.0_re0.0_mask16x16_seqlen16x16_learnemb_zprojtied_wresmlp_L2norm_logitbias_blr1e-4_linear_wu0_wd0.02_gc3_bsz512"
 LOG_PATH="${PROJECT_ROOT}/logs"
 
 ## -----------------------------
@@ -108,9 +108,6 @@ torchrun \
     --gen_freq 5 \
     --gen_bsz 1 \
     --gen_num_images ${NPROC_PER_NODE} \
-    --eval_freq 20 \
-    --eval_bsz 64 \
-    --eval_num_images 5000 \
     --sampling_mode diffusion \
     --num_iter 1 \
     --num_sampling_steps 100 \
